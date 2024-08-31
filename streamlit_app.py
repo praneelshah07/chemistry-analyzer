a0b7db7cb38fec2b786891b2f1256c9298fc4fb1
praneelshah07 <praneelshah101@gmail.com>
2024-08-31 15:04:30 -0700

    Added data visualization

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Chemical Analyzer App")
 
@@ -15,3 +16,11 @@ if uploaded_file is not None:
     # Display the first few rows of the data
     st.write("Here is a preview of your data:")
     st.write(data.head())
+
+    # Plot the data (assuming it has 'Wavelength' and 'Absorbance' columns)
+    st.write("Here is a plot of your data:")
+    fig, ax = plt.subplots()
+    ax.plot(data['Wavelength'], data['Absorbance'])
+    ax.set_xlabel('Wavelength')
+    ax.set_ylabel('Absorbance')
+    st.pyplot(fig)
