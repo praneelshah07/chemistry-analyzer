36f90b6f1b07efbee37177f7d57f634e787d87c8
praneelshah07 <praneelshah101@gmail.com>
2024-08-31 14:50:55 -0700

    Added a button to redirect to Streamlit community

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Welcome to My Chemical Analyzer App")
+
+st.write("This is a simple app for a mini project. Click the button below to visit the Streamlit community!")
+
+if st.button('Go to Streamlit Community'):
+    st.write('Redirecting to the Streamlit Community...')
+    st.markdown('[Streamlit Community](https://discuss.streamlit.io)', unsafe_allow_html=True)
+
